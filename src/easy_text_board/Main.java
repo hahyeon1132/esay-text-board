8207bad3da268af7a0b09e946471d4a47c4e60f0
hahyeon1132 <kogw66@gmail.com>
2021-08-23 18:34:02 +0900

    게시글 2개까지 생성 게시물 리스팅 구현

--- src/easy_text_board/Main.java
+++ src/easy_text_board/Main.java
@@ -7,6 +7,13 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		int lastArticleId = 0;
+		int id1 = 0;
+		int id2 = 0;
+		String title1 = "";
+		String body1 = "";
+		String title2 = "";
+		String body2 = "";
+		
 
 		while (true) {
 			System.out.printf("명령어 : ");
@@ -14,13 +21,24 @@ public class Main {
 
 			if (command.equals("article add")) {
 				System.out.println("== 게시글 작성 ==");
-
+				
+				int id = lastArticleId + 1;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				int id = lastArticleId + 1;
-
+				if(id == 1) {
+					title1 = title;
+					body1 = body;
+					id1 = id;
+				}else if (id == 2) {
+					title2 = title;
+					body2 = body;
+					id2 = id;
+				}else {
+					System.out.println("더 이상 게시글을 생성할 수 없습니다.");
+					continue;
+				}
 				System.out.println("== 입력된 내용 ==");
 				System.out.printf("번호 : %d\n", id);
 				System.out.printf("제목 : %s\n", title);
@@ -30,6 +48,20 @@ public class Main {
 
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시글 리스트 ==");
+				if(lastArticleId == 0) {
+					System.out.println("게시글이 없습니다.");
+				} 
+				else {
+					System.out.println("번호 / 제목 / 내용");
+					if(lastArticleId == 1){
+						System.out.printf("%d / %s / %s\n" , id1 , title1 , body1);
+					}
+					else if(lastArticleId ==2) {
+						System.out.printf("%d / %s / %s\n" , id1 , title1 , body1);
+						System.out.printf("%d / %s / %s\n" , id2 , title2 , body2);
+					}
+					
+				}
 			} else if (command.equals("system exit")) {
 				System.out.println("== 시스템 종료 ==");
 				break;

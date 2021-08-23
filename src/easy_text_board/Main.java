b15f4b07dd10ed809b9f7cd682ebd5b618b85dcc
hahyeon1132 <kogw66@gmail.com>
2021-08-23 17:34:39 +0900

    게시물 등록 구현

--- src/easy_text_board/Main.java
+++ src/easy_text_board/Main.java
@@ -6,21 +6,30 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-
-		String command = "";
-
 		while (true) {
 			System.out.printf("명령어 : ");
-			command = sc.nextLine();
+			String command = sc.nextLine();
+
 			if (command.equals("article add")) {
-				System.out.println("== 게시물 등록 ==");
+				System.out.println("== 게시글 작성 ==");
+				
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				int id = 1;
+				
+				System.out.println("== 입력된 내용 ==");
+				System.out.println("제목 : " + title);
+				System.out.println("내용 : " + body);
+				System.out.println("번호 : " + id);
 			} else if (command.equals("article list")) {
-				System.out.println("== 게시물 목록 ==");
+				System.out.println("== 게시글 리스트 ==");
 			} else if (command.equals("system exit")) {
 				System.out.println("== 시스템 종료 ==");
 				break;
 			} else {
-				System.out.println("== 존재하지 않은 명령어 ==");
+				System.out.println("존재하지 않는 명령어");
 			}
 		}
 		sc.close();

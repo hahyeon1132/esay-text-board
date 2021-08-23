8e71ea1fa56a284e57d779e1150d13de12fec01b
hahyeon1132 <kogw66@gmail.com>
2021-08-23 16:55:48 +0900

    스캐너로 고객에게 명령받기

--- src/easy_text_board/Main.java
+++ src/easy_text_board/Main.java
@@ -1,9 +1,26 @@
 package easy_text_board;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("안녕!");
-		System.out.println("깃연결");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String command = "";
+		
+		System.out.printf("명령어 : ");
+		command = sc.nextLine();
+		
+		if(command.equals("article add")) {
+			System.out.println("== 게시물 등록 ==");
+		}
+		else if(command.equals("article list")) {
+			System.out.println("== 게시물 목록 ==");
+		}else {
+			System.out.println("== 존재하지 않은 명령어 ==");
+		}
+		sc.close();
 	}
 
 }

c6045bba5d907bdc855903843827aaa26f88e1f6
hahyeon1132 <kogw66@gmail.com>
2021-08-23 17:12:24 +0900

    무한루프

--- src/easy_text_board/Main.java
+++ src/easy_text_board/Main.java
@@ -4,23 +4,25 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		String command = "";
-		
-		System.out.printf("명령어 : ");
-		command = sc.nextLine();
-		
-		if(command.equals("article add")) {
-			System.out.println("== 게시물 등록 ==");
-		}
-		else if(command.equals("article list")) {
-			System.out.println("== 게시물 목록 ==");
-		}else {
-			System.out.println("== 존재하지 않은 명령어 ==");
+
+		while (true) {
+			System.out.printf("명령어 : ");
+			command = sc.nextLine();
+			if (command.equals("article add")) {
+				System.out.println("== 게시물 등록 ==");
+			} else if (command.equals("article list")) {
+				System.out.println("== 게시물 목록 ==");
+			} else if (command.equals("system exit")) {
+				System.out.println("== 시스템 종료 ==");
+				break;
+			} else {
+				System.out.println("== 존재하지 않은 명령어 ==");
+			}
 		}
 		sc.close();
 	}
-
 }

8a43b1e49d33b0249dc79c3058b20910ba0902ce
hahyeon1132 <kogw66@gmail.com>
2021-08-23 17:45:50 +0900

    게시물 작성시 번호 증가

--- src/easy_text_board/Main.java
+++ src/easy_text_board/Main.java
@@ -6,23 +6,28 @@ public class Main {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		int lastArticleId = 0;
+
 		while (true) {
 			System.out.printf("명령어 : ");
 			String command = sc.nextLine();
 
 			if (command.equals("article add")) {
 				System.out.println("== 게시글 작성 ==");
-				
+
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				int id = 1;
-				
+				int id = lastArticleId + 1;
+
 				System.out.println("== 입력된 내용 ==");
-				System.out.println("제목 : " + title);
-				System.out.println("내용 : " + body);
-				System.out.println("번호 : " + id);
+				System.out.printf("번호 : %d\n", id);
+				System.out.printf("제목 : %s\n", title);
+				System.out.printf("내용 : %s\n", body);
+
+				lastArticleId = id;
+
 			} else if (command.equals("article list")) {
 				System.out.println("== 게시글 리스트 ==");
 			} else if (command.equals("system exit")) {

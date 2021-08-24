5eb0489995b5fc7623f739e46060a08033e5a8ca
hahyeon1132 <kogw66@gmail.com>
2021-08-24 16:31:36 +0900

    개시글 생성과 리스트 구조 개선

--- src/easy_text_board/App.java
+++ src/easy_text_board/App.java
@@ -28,23 +28,23 @@ public class App {
 			if (command.equals("article add")) {
 				System.out.println("== 게시글 작성 ==");
 
+				if (lastArticleId == 2) {
+					System.out.println("더 이상 개시글을 생성할 수 없습니다.");
+					continue;
+				}
+				
 				int id = lastArticleId + 1;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				if (id == 1) {
-					article1.title = title;
-					article1.body = body;
-					article1.id = id;
-				} else if (id == 2) {
-					article2.title = title;
-					article2.body = body;
-					article2.id = id;
-				} else {
-					System.out.println("더 이상 게시글을 생성할 수 없습니다.");
-					continue;
-				}
+
+				Article article = getArticle(id);
+
+				article.id = id;
+				article.title = title;
+				article.body = body;
+
 				System.out.println("== 입력된 내용 ==");
 				System.out.printf("번호 : %d\n", id);
 				System.out.printf("제목 : %s\n", title);
@@ -57,14 +57,11 @@ public class App {
 				if (lastArticleId == 0) {
 					System.out.println("게시글이 없습니다.");
 				} else {
-					System.out.println("번호 / 제목 / 내용");
-					if (lastArticleId == 1) {
-						System.out.printf("%d / %s / %s\n", article1.id, article1.title, article1.body);
-					} else if (lastArticleId == 2) {
-						System.out.printf("%d / %s / %s\n", article1.id, article1.title, article1.body);
-						System.out.printf("%d / %s / %s\n", article2.id, article2.title, article2.body);
+					System.out.println("번호 / 제목");
+					for (int i = 1; i <= lastArticleId; i++) {
+						Article article = getArticle(i);
+						System.out.printf("%s / %s\n", article.id, article.title);
 					}
-
 				}
 			} else if (command.equals("system exit")) {
 				System.out.println("== 시스템 종료 ==");
@@ -72,11 +69,10 @@ public class App {
 			} else if (command.startsWith("article detail ")) {
 				int inputedId = Integer.parseInt(command.split(" ")[2]);
 				Article selectedArticle = getArticle(inputedId);
-				
-				if (inputedId > lastArticleId || inputedId == 0) {
+				if (selectedArticle == null || selectedArticle.id == 0) {
 					System.out.printf("%d번 개시글이 존재하지 않습니다.\n", inputedId);
 					continue;
-				} 
+				}
 				System.out.printf("번호 : %d\n", selectedArticle.id);
 				System.out.printf("제목 : %s\n", selectedArticle.title);
 				System.out.printf("내용 : %s\n", selectedArticle.body);
